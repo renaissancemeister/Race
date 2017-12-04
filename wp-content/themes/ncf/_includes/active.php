3749139b40363cdba8e25329dc15064a03959bed
Kevin McCormick <admin@nc-f.org>
2017-12-04 12:32:58 -0800

    initial re-commit

---
+++ wp-content/themes/ncf/_includes/active.php
@@ -0,0 +1,8 @@
+<!-- begin active.php -->
+<script>
+jQuery( document ).ready( function( $ ) {
+	$( '#race' ).addClass( 'active' );
+	$( '#raceorg' ).addClass( 'active' );
+});
+</script>
+<!-- end active.php -->
---
+++ wp-content/themes/ncf/_includes/navbar.php
@@ -0,0 +1,32 @@
+<!-- begin navbar.php -->
+	<div class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="sr-only">Toggle navigation</span> Menu <span class="fa fa-list fa-lg"></span> </button> <a class="navbar-brand" href="http://www.nc-f.org/index.php">NCF</a>
+			</div><!-- /.navbar-header -->
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class="dropdown"><a href="http://www.amren.com/" class="dropdown-toggle" data-toggle="dropdown"> American Renaissance <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="http://www.amren.com/">News</a></li>
+						<li><a href="http://store.amren.com/">Store</a></li>
+					</ul>
+					</li>
+					<li class="dropdown" id="crime"><a href="http://www.colorofcrime.com/" class="dropdown-toggle" data-toggle="dropdown"> Crime <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li id="coc"><a href="http://www.colorofcrime.com/">Color of Crime</a></li>
+						<li id="ec"><a href="http://www.ethniccrime.com/">Ethnic Crime</a></li>
+					</ul>
+					</li>
+					<li id="jt"><a href="http://www.jaredtaylor.org/">Jared Taylor</a></li>
+					<li class="dropdown" id="race"> <a href="http://www.race.org/" class="dropdown-toggle" data-toggle="dropdown">Race <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li id="acar"><a href="http://www.aconversationaboutrace.com/">A Conversation about Race</a></li>
+						<li id="raceorg"><a href="http://www.race.org/">Race.org</a></li>
+					</ul>
+					</li>
+				</ul>
+			</div><!--/.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</div><!-- /.navbar -->
+<!-- end navbar.php -->
