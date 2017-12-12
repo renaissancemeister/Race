d0c518e9fe1ba495bf498523afa60d2e3d6548fe
Kevin McCormick <admin@nc-f.org>
2017-12-12 15:46:30 -0800

    remove old WP cruft

--- wp-content/themes/ncf/_includes/active.php
+++
@@ -1,8 +0,0 @@
-<!-- begin active.php -->
-<script>
-jQuery( document ).ready( function( $ ) {
-	$( '#race' ).addClass( 'active' );
-	$( '#raceorg' ).addClass( 'active' );
-});
-</script>
-<!-- end active.php -->
